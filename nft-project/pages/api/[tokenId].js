aa485e199666868156c7e07f426c6ff87aaf9cd7
Elisha Bere <elishabere4@gmail.com>
2022-07-23 15:52:12 +0200

    add api

--- nft-project/pages/api/[tokenId].js
+++ nft-project/pages/api/[tokenId].js
@@ -1,7 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  // get the tokenId from the query params
+  // get the tokenId from the query params -
   const tokenId = req.query.tokenId;
   // As all the images are uploaded on github, we can extract the images from github directly.
   const image_url =
